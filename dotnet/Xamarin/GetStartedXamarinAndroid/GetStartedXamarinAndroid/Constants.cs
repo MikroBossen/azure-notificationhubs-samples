f614f50628c9fb3df32a92ff97e19b7856634ae5
Admin <Admin>
2016-09-22 09:21:58 +0200

    Andres kode

---
+++ dotnet/Xamarin/GetStartedXamarinAndroid/GetStartedXamarinAndroid/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GetStartedXamarinAndroid
+{
+
+	public static class Constants
+	{
+        public const string SenderID = "828768700042"; // Google API Project Number
+        public const string ListenConnectionString = "Endpoint=sb://bosspushnoticetest.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=rpyz5KCmuuiGk6GHX8jGi2pfxgaFuoIjEnm8bXRT16Q=";
+        public const string NotificationHubName = "bosstestnoticehub";
+    }
+}
+
